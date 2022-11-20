81ba1ca56a11c38b4a77a348511b0b6ae4af6f6c
Nathan Lloyd <nathan.lloyd.7003@gmail.com>
2022-11-19 21:04:13 -0400

    Added ability to double jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,13 @@ public class PlayerController : MonoBehaviour
     private CharacterController _controller;
 
     [SerializeField] private float _yVelocity = 0.0f;
+    [SerializeField] private bool _canDoubleJump;
 
     [Header("Player Stats")]
     [SerializeField] private float _moveSpeed = 5.0f;
     [SerializeField] private float _gravity = 1.0f;
     [SerializeField] private float _jumpHeight = 15.0f;
+    [SerializeField] private float _doubleJumpHeight = 10.5f;
 
     void Start()
     {
@@ -20,7 +22,7 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        
+        PlayerJump();
     }
 
     private void FixedUpdate()
@@ -35,23 +37,27 @@ public class PlayerController : MonoBehaviour
         Vector3 direction = new Vector3(horizontalInput, 0);
         Vector3 velocity = direction * _moveSpeed;
 
-        if(_controller.isGrounded == true)
-        {
-            if(Input.GetKey(KeyCode.Space))
-            {
-                _yVelocity = _jumpHeight;
-            }
-            else
-            {
-                _yVelocity = 0;
-            }
-        }
-        else
+
+        if (_controller.isGrounded == false)
         {
             _yVelocity -= _gravity;
         }
 
         velocity.y = _yVelocity;
         _controller.Move(velocity * Time.deltaTime);
+    }
+
+    void PlayerJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _controller.isGrounded == true)
+        {
+            Debug.Log("Space key has been pressed!");
+            _yVelocity = _jumpHeight;
+            _canDoubleJump = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && _controller.isGrounded == false && _canDoubleJump == true)
+        {
+            _yVelocity += _doubleJumpHeight;
+        }
     }
 }
